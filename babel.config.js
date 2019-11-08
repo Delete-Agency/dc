fc7832e2d0a777d89887c33ac74af19e16c84c0a
anton-github <shockfront00@gmail.com>
2019-11-08 11:33:25 +0500

    publish without bundling

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,20 @@
 module.exports = {
     presets: [
-        ['@babel/preset-env']
-    ],
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: 2,
+                targets: [
+                    "ie 11",
+                    "last 2 firefox versions",
+                    "last 2 edge versions",
+                    "last 2 chrome versions",
+                    "last 2 safari versions",
+                    "last 2 and_chr versions",
+                    "last 2 ios_saf versions"
+                ]
+            }
+        ]
+    ]
 };
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -22,17 +22,6 @@ module.exports = (env, argv) => {
                     exclude: /node_modules/,
                     use: {
                         loader: "babel-loader",
-                        options: {
-                            presets: [
-                                [
-                                    '@babel/preset-env',
-                                    {
-                                        useBuiltIns: 'usage',
-                                        corejs: 3
-                                    }
-                                ]
-                            ]
-                        }
                     }
                 }
             ]
